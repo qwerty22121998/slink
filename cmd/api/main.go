dcd0f5669f3bde8e4f8281e9ca7d4a018bf4c0d5
vu.ho <qwerty22121998@gmail.com>
2022-01-20 11:15:00 +0700

    chore: doc

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,32 +13,10 @@ func main() {
 	config := configs.Global
 	e := echo.New()
 	route := e.Group("/api")
-
 	fb := firebase.NewFireBaseAdapter(firebase.Config{
 		CertPath: config.GoogleAppCertPath,
 	})
-
 	repo := repositories.NewShortLinkRepository(fb.DB)
-
-	//c, err := repo.Find(context.Background(), "ok")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Printf("%#v", c)
-
-	//now := time.Now()
-	//if _, err := repo.Set(context.Background(), "facebook", &models.ShortLink{
-	//	Name:       "facebook",
-	//	URL:        "https://www.facebook.com/",
-	//	Short:      "facebook",
-	//	ExpiredAt:  &now,
-	//	ClickCount: 0,
-	//}); err != nil {
-	//	panic(err)
-	//}
-	//
-	//return
-
 	service := services.NewShortLinkService(repo)
 
 	slController := controllers.NewShortLinkController(service)
